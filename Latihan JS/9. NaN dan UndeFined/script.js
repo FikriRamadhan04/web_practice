e9c18c8168a677a60a13c47214a3123e66ede88d
Fikri Ramadhan <fikriramadhan1152@gmail.com>
2025-09-25 15:22:13 +0700

    update

---
+++ Latihan JS/9. NaN dan UndeFined/script.js
@@ -0,0 +1,17 @@
+// NaN dan UndeFined
+
+// NaN = Not a Number
+console.log(`akar dari -1 :${Math.sqrt(-1)}`);
+console.log(`"ucup" / "otong" :${"ucup" / "otong"}`);
+console.log(`"ucup" * "otong" :${"ucup" * "otong"}`);
+console.log(`"ucup" + "otong" :${"ucup" + "otong"}`);
+console.log(`"ucup" - "otong" :${"ucup" - "otong"}`);
+
+let data = parseInt("test123");
+console.log(data + 5); //NaN ini tidak bisa dioperasikan lagi
+
+// UndeFined - Dia belum tau nilainya
+let a = 0;
+console.log(`a = ${a}`);
+console.log(Math.sqrt(4));
+console.log(console.log("test"));
