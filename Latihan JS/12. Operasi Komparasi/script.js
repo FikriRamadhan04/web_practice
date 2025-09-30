53b380e97f92aad8e2db783980630369a777de89
Fikri Ramadhan <fikriramadhan1152@gmail.com>
2025-09-30 11:03:32 +0700

    update

---
+++ Latihan JS/12. Operasi Komparasi/script.js
@@ -0,0 +1,30 @@
+// Operator Komparasi (Membandingkan 2 buah nilai)
+
+// == equality
+// != inequality
+// === strict equality
+// !== strict inequality
+// lebih besar dari
+// >= lebih besar sama dengan
+// < lebih kecil dari
+// <= lebih kecil sama dengan
+
+console.log(`10 >7 = ${10 > 7}`);
+let dataBool = 10 > 7;
+console.log(`10 > 7 =${dataBool}`);
+
+console.log(`10 >= 10 = ${10 >= 10}`);
+console.log(`7 < 10 = ${7 < 10}`);
+console.log(`7 <= 7 = ${7 <= 7}`);
+
+// Persamaan
+console.log(`3 == "3" = ${3 == "3"}`);
+console.log(`3 != 3 = ${3 != "3"}`);
+
+console.log(`3 === 3 = ${3 === 3}`);
+console.log(`3 === "3" = ${3 === "3"}`);
+console.log(`"3" === "3" = ${"3" === "3"}`);
+
+console.log(`3 !== 3 = ${3 !== 3}`);
+console.log(`3 !== "3" = ${3 !== "3"}`);
+console.log(`"3" 1== "3" = ${"3" !== "3"}`);
